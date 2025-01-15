e3212952948a5b3ae085c3a93ecdfb0e47b2a356
Alan-David-Racines <aracines@unal.edu.co>
2025-01-14 22:50:36 -0500

    Update Asiento.java

--- src/gestionAplicacion/proyecciones/Asiento.java
+++ src/gestionAplicacion/proyecciones/Asiento.java
@@ -1,1 +1,34 @@
+package gestionAplicacion.proyecciones;
 
+import java.io.Serializable;
+
+/**
+ * @author Alan David Racines Casierra
+ * */
+public class Asiento implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String numeroAsiento;
+	private boolean disponibilidad = true;
+	
+	public Asiento(int fila, int columna){
+		this.numeroAsiento = (fila + 1) + "-" + (columna + 1);
+	}
+	public String getNumeroAsiento() {
+		return numeroAsiento;
+	}
+
+	public void setNumeroAsiento(String numeroAsiento) {
+		this.numeroAsiento = numeroAsiento;
+	}
+
+	public boolean isDisponibilidad() {
+		return disponibilidad;
+	}
+
+	public void setDisponibilidad(boolean disponibilidad) {
+		this.disponibilidad = disponibilidad;
+	}
+	
+}
