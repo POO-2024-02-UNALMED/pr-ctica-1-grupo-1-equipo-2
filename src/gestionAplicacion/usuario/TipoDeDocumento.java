1418199264a92bf1ef5a9ca2035fd65004678c37
Alan-David-Racines <aracines@unal.edu.co>
2025-01-14 22:27:35 -0500

    Update TipoDeDocumento.java

--- src/gestionAplicacion/usuario/TipoDeDocumento.java
+++ src/gestionAplicacion/usuario/TipoDeDocumento.java
@@ -1,1 +1,13 @@
+package gestionAplicacion.usuario;
 
+/**
+*@author Alan David Racines Casierra
+**/
+public enum TipoDeDocumento {
+
+CC(nombre:"cedula de ciudadania), TI(nombre:"Tarjeta de identidad"), CE(nombre:"cedula de extranjeria");
+
+private String nombre;
+private TipoDeDocumento(String nombre) {this.nombre = nombre;}
+
+}
