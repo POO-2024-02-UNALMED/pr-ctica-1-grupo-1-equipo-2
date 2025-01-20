a3dcd31cd5aa6dc57b999afb2beece549ee7eb36
Alan-David-Racines <aracines@unal.edu.co>
2025-01-19 23:14:24 -0500

    Update Cliente.java

--- src/gestionAplicacion/usuario/Cliente.java
+++ src/gestionAplicacion/usuario/Cliente.java
@@ -13,3 +13,38 @@ import gestionAplicacion.servicios.Producto;
 /**
 * @author Todos los integrantes del equipo participaron en la construcción de esta clase
  * */
+public class Cliente implements Serializable{
+
+  private static final long serialVersionUID = 1L;
+  
+  //Atributos
+  private String nombre;
+  private int edad;
+  private long documento;
+  private TipoDeDocumento tipoDocumento;
+  private SucursalCine cineActual;
+
+  //Atributos para funcionalidad 1 
+  private ArrayList<Ticket> tickets = new ArrayList<>();
+  private ArrayList<Pelicula> historialDePeliculas = new ArrayList<>();
+
+  //Atributos para funcionalidad 3
+  private ArrayList<Pelicula> peliculasDisponiblesParaCalificar = new arrayList<>();
+  private ArrayList<Producto> productosDisponiblesParaCalificar = new arrayList<>();
+
+  //Atributos para funcionalidad 4 y 2 
+  private TarjetaCinemar cuenta;
+  private ArrayList<String> codigosDescuento = new ArrayList<>();
+  private ArrayList<String> codigosBonos = new ArrayList<>();
+  private ArrayList<Bono> bonos = new ArrayList<>();
+  private ArrayList<Producto> historialDePedidos = new ArrayList<>();
+
+  //Atributos para funcionalidad 5 
+  private Membresia membresia;
+  private LocalDate fechaLimiteMembresia;
+  private int puntos;
+  private int origenMembresia;
+  private ArrayList<MetodoPago> metodosDePago = new ArrayList<>();
+
+  //Constructores 
+}
