93e51c1ad5c51f63a3aeac322827fbf0c575c8ae
Alan-David-Racines <aracines@unal.edu.co>
2025-01-19 21:48:43 -0500

    Update Cliente.java

--- src/gestionAplicacion/usuario/Cliente.java
+++ src/gestionAplicacion/usuario/Cliente.java
@@ -1,1 +1,15 @@
+package gestionAplicacion.usuario;
+import java.util.ArrayList;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Iterator;
 
+import gestionAplicacion.SucursalCine;
+import gestionAplicacion.proyecciones.Pelicula;
+import gestionAplicacion.servicios.Bono;
+import gestionAplicacion.servicios.Producto;
+//import gestionAplicacion.servicios.Servicio;
+
+/**
+* @author Todos los integrantes del equipo participaron en la construcción de esta clase
+ * */
